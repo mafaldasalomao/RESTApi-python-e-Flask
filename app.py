adf15f1a73d65e05c21823b35903cfacac37aed8
Salomao Machado Mafalda <salomaomachadon@gmail.com>
2022-03-12 21:30:15 -0500

    Insert hoteis

--- app.py
+++ app.py
@@ -4,7 +4,29 @@ from flask_restful import Resource, Api
 app= Flask(__name__)
 api = Api(app)
 
-
+hoteis = [
+    {
+        'hotel_id': 'alpha',
+        'nome':'alpha hotel',
+        'estrelas': 4.5,
+        'diaria': 343.00,
+        'cidade':'Rio Branco'
+    },
+    {
+        'hotel_id': 'beta',
+        'nome':'beta hotel',
+        'estrelas': 4,
+        'diaria': 400.00,
+        'cidade':'Rio Branco'
+    },
+    {
+        'hotel_id': 'charlie',
+        'nome':'charlie hotel',
+        'estrelas': 4.6,
+        'diaria': 300.00,
+        'cidade':'Rio Branco'
+    }
+]
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': 'Meus hoteis'}
