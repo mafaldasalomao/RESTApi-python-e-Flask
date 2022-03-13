830a18ba1c29c8c6f716c9c0a7a305af004dd7de
Salomao Machado Mafalda <salomaomachadon@gmail.com>
2022-03-13 18:16:36 -0500

    Upload folder resource

--- app.py
+++ app.py
@@ -1,35 +1,11 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import  Api
+from resources.hotel import Hoteis
+
+
 
 app= Flask(__name__)
 api = Api(app)
-
-hoteis = [
-    {
-        'hotel_id': 'alpha',
-        'nome':'alpha hotel',
-        'estrelas': 4.5,
-        'diaria': 343.00,
-        'cidade':'Rio Branco'
-    },
-    {
-        'hotel_id': 'beta',
-        'nome':'beta hotel',
-        'estrelas': 4,
-        'diaria': 400.00,
-        'cidade':'Rio Branco'
-    },
-    {
-        'hotel_id': 'charlie',
-        'nome':'charlie hotel',
-        'estrelas': 4.6,
-        'diaria': 300.00,
-        'cidade':'Rio Branco'
-    }
-]
-class Hoteis(Resource):
-    def get(self):
-        return {'hoteis': 'Meus hoteis'}
 
 api.add_resource(Hoteis, '/hoteis')
 
---
+++ resources/hotel.py
@@ -0,0 +1,28 @@
+from flask_restful import Resource
+
+hoteis = [
+    {
+        'hotel_id': 'alpha',
+        'nome':'alpha hotel',
+        'estrelas': 4.5,
+        'diaria': 343.00,
+        'cidade':'Rio Branco'
+    },
+    {
+        'hotel_id': 'beta',
+        'nome':'beta hotel',
+        'estrelas': 4,
+        'diaria': 400.00,
+        'cidade':'Rio Branco'
+    },
+    {
+        'hotel_id': 'charlie',
+        'nome':'charlie hotel',
+        'estrelas': 4.6,
+        'diaria': 300.00,
+        'cidade':'Rio Branco'
+    }
+]
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis': 'Meus hoteis'}
