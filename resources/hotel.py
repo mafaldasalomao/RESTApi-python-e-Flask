7517d22770d603557cabc928fd6ad3c8b90f4810
Salomao Machado Mafalda <salomaomachadon@gmail.com>
2022-03-14 14:06:13 -0500

    change venv

--- resources/hotel.py
+++ resources/hotel.py
@@ -26,3 +26,13 @@ hoteis = [
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
+
+class Hotel(Resource):
+    def get(self):
+        pass
+    def post(self):
+        pass
+    def put(self):
+        pass
+    def delete(self):
+        pass
